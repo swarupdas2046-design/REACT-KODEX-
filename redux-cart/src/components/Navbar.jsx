5c55be268133970ae583dd0031b66c5fdff47e34
Swarup Das <swarupdas2046@gmail.com>
2026-04-24 10:06:51 +0530

    Some Changes on Navbar

--- redux-cart/src/components/Navbar.jsx
+++ redux-cart/src/components/Navbar.jsx
@@ -21,6 +21,9 @@ const Navbar = () => {
         
         <TbMenuDeep size={30} className='lg:hidden md:hidden' onClick={()=>{setToggle(!toggle)}} />
           {toggle&& <div className='absolute top-[10%] flex flex-col items-center p-6 gap-4 z-30 right-0 w-[70%] h-full bg-pink-400 transform-translate-x-0 ease-in-out duration-300'> 
+
+            {Theme?<MdDarkMode size={30} onClick={()=>{dispatch(themeChange(false))}}/>:<CiLight size={30} onClick={()=>{dispatch(themeChange(true))}} />}
+
             <NavLink onClick={()=>{setToggle(false)}} className={({isActive})=>isActive?'text-2xl font-semibold hover:underline text-amber-400':'text-2xl font-semibold'} to='/'>Home</NavLink>
             <NavLink onClick={()=>{setToggle(false)}} className={({isActive})=>isActive?'text-2xl font-semibold hover:underline text-amber-400':'text-2xl font-semibold'} to='/about'>About</NavLink>
             <NavLink onClick={()=>{setToggle(false)}} className={({isActive})=>isActive?'text-2xl font-semibold hover:underline text-amber-400':'text-2xl font-semibold'} to='/shop'>Shop</NavLink>
@@ -29,10 +32,10 @@ const Navbar = () => {
         
         </div>}
       
-      <div className='flex items-center gap-4'>
+      <div className='hidden md:flex items-center gap-4'>
       {Theme?<MdDarkMode size={30} onClick={()=>{dispatch(themeChange(false))}}/>:<CiLight size={30} onClick={()=>{dispatch(themeChange(true))}} />}
 
-        <button className='hidden md:block bg-amber-400 px-4 py-2 rounded active:scale-95 font-bold cursor-pointer'>Sign up</button>
+        <button className=' bg-amber-400 px-4 py-2 rounded active:scale-95 font-bold cursor-pointer'>Sign up</button>
       </div>
        
     </div>
